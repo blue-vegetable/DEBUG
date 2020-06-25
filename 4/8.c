a276ab0fc530be685475c4640a5df811629aef81
zyx <1250897150@qq.com>
2020-06-25 20:51:50 +0800

    debug4-8

--- 4/8.c
+++ 4/8.c
@@ -5,24 +5,20 @@ int main() {
     int m;
     int numbers[10];
     int i;
-    int max;
-    int inter;
-    // 读入给定的数字
+    int temp;
+    
     for (i = 0; i < n; i++) {
         scanf("%d", &numbers[i]);
     }
     for(i = 0; i <= 9; i++){
-        for(m = i; m <= 9; m++){
-            max = m;
-            if(numbers[max] < numbers[m+1]){
-                max = m + 1;
-            }
-        }
-        inter = numbers[i];
-        numbers[i] = numbers[max];
-        numbers[max] = numbers[i];
+        for(m = 0; m < 9-i; m++){
+            if(numbers[m] < numbers[m+1]){
+                       temp=numbers[m];
+                       numbers[m]=numbers[m+1];
+                       numbers[m+1]=temp;
+                   }
     }
-    
+    }
     for(i = 0; i <= 9; i++){
         printf("%d", numbers[i]);
         if(i != 9){
