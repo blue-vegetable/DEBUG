e4cfdc8c8aa7a3463bd5804c5889c738d99a8a74
zyx <1250897150@qq.com>
2020-06-26 17:09:16 +0800

    debug5-9

--- 5/9.c
+++ 5/9.c
@@ -2,22 +2,25 @@
 #include <string.h>
 int main() {
     int i,j;
-    char name[10][20]={0};
-    for(i = 0,j = 0; i < 10; i++){
-        scanf("%s",&name[i][j]);
+    char name[10][20]={0},m[21];
+    for(i = 0; i < 10; i++){
+        scanf("%s",name[i]);
     }
     
-    for(i = 0,j = 0; i < 9; i++){
-        if(strcmp(&name[i][j],&name[i+1][j]) > 0){
-            char temp[10];
-            strcpy(temp,&name[i][j]);
-            strcpy(&name[i][j],&name[i+1][j]);
-            strcpy(&name[i+1][j],&temp);
-        }
+    for(i = 0; i < 9; i++){
+       for(j=i+1;j<10;j++)
+         {
+             if(strcmp(name[i],name[j])>0)
+             {
+        strcpy(m,name[i]);
+        strcpy(name[i],name[j]);
+        strcpy(name[j],m);
+             }
+         }
     }
 
-    for(i = 0,j = 0; i < 10; i++){
-        printf("%s\n",&name[i][j]);
+    for(i = 0; i < 10; i++){
+        printf("%s\n",name[i]);
     }
     
     return 0;
