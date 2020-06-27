d27994cfbc245d937c3b7c2f790a9d008bcc83e4
zyx <1250897150@qq.com>
2020-06-27 22:02:33 +0800

    debug 11-10

--- 11/10.c
+++ 11/10.c
@@ -2,25 +2,24 @@
 #include<string.h>
 const int maxn = 10010;
 int main() {
-    int i,j,k;
+    int i,j=0;
     int arr[32]={0};
     int bits[32];
     char str[maxn],rstr[maxn];
-    i=0;
-    while(scanf("%c",&str[i])!=EOF){
-        if(str[i]=="\n"){
-            str[i]='\0';
+    scanf("%s",str);
+ 
+    while (str[j] != '\0') {
+        if (str[j] == '\n') {
             break;
         }
-        i++;
+        arr[(j + 1) % 32] = arr[(j + 1) % 32] + (int)(str[j]);
+        j++;
     }
-    k=strlen(str);
-    for(i=0;i<k;i++){
-        arr[i%31]+=str[i];
-    }
-    for(j=0;j<32;j++){
-        bits[j]=arr[31-j]^(arr[j]<<1);
-        rstr[j]=bits[j]%85+34;
+
+    for (int i = 0; i < 32; i++) {
+        bits[i] = (arr[31 - i]) ^ (arr[i] << 1);
+        int b = (bits[i] % 85) + 34;
+        rstr[i] = (char)b;
     }
     for(i=0;i<32;i++){
         printf("%c",rstr[i]);
