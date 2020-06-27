8649465f799a1fa040ad3a85264d1830142d4afd
zyx <1250897150@qq.com>
2020-06-27 20:59:49 +0800

    debug 11-7

--- 11/7.c
+++ 11/7.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-    char str[500], fs[32], arr[32], bits[32];
-    int i;
+    char str[500], fs[32];
+    int  arr[32], bits[32];
+    int i,j=0;
     memset(arr, 0, sizeof(arr));
     memset(fs, '\0', sizeof(fs));
     scanf("%s", str);
-    for (i = 0; i < strlen(str); i++)
-    {
-        if (str[i] == '\n') break;
-        arr[i % 32] += str[i];
+    while (str[j] !='\0') {
+        if (str[j] == '\n') {
+                   break;
+               }
+    arr[(j + 1) % 32] = arr[(j + 1) % 32] + (int)(str[j]);
+    j++;
     }
     for (i = 0; i < 32; i++)
     {
         bits[i] = arr[31 - i] ^ (arr[i] << 1);
-        fs[i] = bits[i] % 85 + 34;
+        int x=(bits[i] % 85) + 34;
+        fs[i] =(char) x;
     }
-    printf("%s", fs);
+    for (int i = 0; i < 32; i++) {
+        printf("%c",fs[i]);
+    }
 
     return 0;
 }
