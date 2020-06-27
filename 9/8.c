70b02329269c4e1b0e31753788dca7b37b4bd6f6
zyx <1250897150@qq.com>
2020-06-27 18:11:25 +0800

    debug 9-8

--- 9/8.c
+++ 9/8.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
-
 int main() {
-    int n;
-    scanf("%d",&n);
-    int i=0;
-    int x;
-    int y;
-    int m=1;
-    int b=1;
-    for(x = 0; x <= n / 2; x++){
-        for(y = 0; y <= n / 3; y++){
-            if(x * 2 + y * 3 == n){
-                i++;
-            }
-        }
+    int N;
+    int i;
+    int part[50];
+    part[2]=1;
+    part[3]=1;
+    part[4]=1;
+    scanf("%d",&N);
+    if (N<=4) {printf("%d",part[N]);return 0;}
+    for(i=5;i<N;i++){
+        part[i]=part[i-2]+part[i-3];
     }
-    printf("%d",i);
+    printf("%d",part[N-2]+part[N-3]);
     return 0;
 }
