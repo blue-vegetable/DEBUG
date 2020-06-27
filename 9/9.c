bb2453704e7f104fc9ac4d43adfec2bc8075f85b
zyx <1250897150@qq.com>
2020-06-27 18:18:38 +0800

    debug 9-9

--- 9/9.c
+++ 9/9.c
@@ -5,21 +5,20 @@ int main() {
     scanf("%d",&n);
     
     int a[51];
-    if(n==2){
-        printf("1");
-    }
-    if(n==3){
-        printf("1");
-    }if(n>3){
     a[2]=1;
     a[3]=1;
-    a[1]=0;
-        int i;
-    for( i=4;i<=n;i++){
+    a[4]=1;
+   // a[1]=0;
+    if(n<=4){
+        printf("%d",a[n]);return 0;
+    }
+    int i;
+    for( i=5;i<n;i++){
         a[i]=a[i-2]+a[i-3];
-        
     }
-        printf("%d",a[i]);
-    }
+    
+        printf("%d",a[n-2]+a[n-3]);
+    
     return 0;
 }
+
