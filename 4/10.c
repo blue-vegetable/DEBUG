9560d5b0c78475e621c20aa16076e3151cb247b6
sati370 <60167827+sati370@users.noreply.github.com>
2020-06-28 17:54:05 +0800

    commit 4-6、7、8、9、10

---
+++ 4/10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 10;
+    int m;
+    int numbers[10];
+    int i;
+    int temp;
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+    for (i = 0; i < n; i++) {
+        for(m = 0; m < 9 - i; m++) {
+            if(numbers[m] < numbers[m + 1]) {
+                temp = numbers[m];
+                numbers[m] = numbers[m + 1];
+                numbers[m + 1] = temp;
+            }
+        }
+    }
+    for (int q = 0; q < n; q++) {
+        if (q == 9) {
+            printf("%d", numbers[q]);
+        } else {
+            printf("%d ", numbers[q]);
+        }
+    }
+
+    return 0;
+}
---
+++ 4/6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 10;
+    int numbers[10];
+    int i, j;
+    int tmpt;
+
+    // 读入给定的数字
+    for (i = 0; i < n; i++) {
+        scanf("%d", &numbers[i]);
+    }
+    for (i = 0; i < n; i++) {
+        for (j = i; j < 9 - n; j++) {
+            if (numbers[i] < numbers[j]) {
+                tmpt = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] =   tmpt;
+            }
+        }
+    }
+    for (int m = 0; m < 10; m++) {
+        if (m != 9) {
+            printf("%d ", numbers[m]);
+        } else {
+            printf("%d", numbers[m]);
+        }
+
+    }
+
+    return 0;
+}
