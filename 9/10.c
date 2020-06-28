f5d65c4de745c9f13c92ddf19d61fcdf8a499363
sati370 <60167827+sati370@users.noreply.github.com>
2020-06-28 18:05:31 +0800

    commit 9-6、7、8、9、10

---
+++ 9/10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int f(int n)
+{
+    if(n <= 4) {
+        return 1;
+    } else {
+        return f(n - 2) + f(n - 3);
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", f(n));
+    return 0;
+}
---
+++ 9/9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int a[51];
+    a[2] = 1;
+    a[3] = 1;
+    a[4] = 1;
+
+    if(n <= 4) {
+        printf("%d", a[n]);
+        return 0;
+    }
+
+    int i;
+    for( i = 5; i < n; i++) {
+        a[i] = a[i - 2] + a[i - 3];
+    }
+
+    printf("%d", a[n - 2] + a[n - 3]);
+
+    return 0;
+}
+
