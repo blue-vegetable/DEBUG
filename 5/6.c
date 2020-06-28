25fc0b3d28c47eaacbf4b39715b71e66ad94bf39
yjq <blues@qq.com>
2020-06-28 19:56:48 +0800

    change some styles

--- 5/6.c
+++ 5/6.c
@@ -6,8 +6,9 @@ int main()
     int i, k;
     char n[10][21], m[21];
 
-    for(i = 0; i < 10; i++)
+    for(i = 0; i < 10; i++) {
         scanf("%s", n[i]);
+	}
     for(i = 0; i < 9; i++) {
         for(k = i + 1; k < 10; k++) {
             if(strcmp(n[i], n[k]) > 0) {
