3ca30e2be7bb974377fea9a6a36e6024028443f4
yjq <blues@qq.com>
2020-06-28 20:11:02 +0800

    debug 1 by yjq

---
+++ 9/1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    int N;
+    scanf("%d", &N);
+    int step[51];
+    step[0] = 0;
+    step[1] = 0;
+    step[2] = 1;
+    step[3] = 1;
+
+    for (int i = 4; i <= N; i++) {
+        step[i] = step[i - 2] + step[i - 3];
+    }
+
+    printf("%d", step[N]);
+    return 0;
+}
+
