d5b048b2a7a75fba117e2fbf0291e7ac6eb2cb53
yjq <blues@qq.com>
2020-06-28 20:20:50 +0800

    debug 4 by yjq

---
+++ 12/4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define PI 3.14159
+
+int main() 
+{
+    double r;
+    
+    scanf("%lf", &r);
+    printf("%.3lf\n", 2 * PI * r);
+    printf("%.3lf", PI * r * r);
+    
+    return 0;
+}
+
