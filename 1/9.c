8aec85ba5ffed0dfec9ca383295a1fdb3d09cd0b
sati370 <60167827+sati370@users.noreply.github.com>
2020-06-28 17:43:20 +0800

    commit 1-6、7、8、9、10

--- 1/9.c
+++ 1/9.c
@@ -4,27 +4,36 @@
 
 double bisection(int p, int q, double (*func)(int, int, double));
 double f(int p, int q, double x);
-int main() {
+
+int main()
+{
     int p;
     int q;
+
     scanf("%d%d", &p, &q);
     printf("%.4f\n", bisection(p, q, f));
+
     return 0;
 }
 
-double bisection(int p, int q, double (*func)(int, int, double)) {
-    double a=-20;
-    double b=20;
-    int p;
-    int q;
-    while(fabs(f(p,q,a+b/2))<=EPSILON)
-   { if(f(p,q,a+b/2<)<0)
-        a=a+b/2;
-    else
-        b=a+b/2;}
-    return f(p,q,a+b/2)
+double bisection(int p, int q, double (*func)(int, int, double))
+{
+    double a = -20;
+    double b = 20;
+    double x = (a + b) / 2;
+
+    while(fabs(f(p, q, x)) > EPSILON) {
+        if(f(p, q, x)*f(p, q, b) < 0)
+            a = x;
+        else
+            b = x;
+        x = (a + b) / 2;
+    }
+
+    return x;
 }
 
-double f(int p, int q, double x) {
+double f(int p, int q, double x)
+{
     return p * x + q;
-}s
+}
