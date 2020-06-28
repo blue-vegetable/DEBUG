ccce118d83c84f8450617e16d6c709d31f23991e
yjq <blues@qq.com>
2020-06-28 18:11:39 +0800

    debug 5 by yjq

---
+++ 8/5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+    getchar();
+    char name[n][100];
+
+    for (int i = 0; i < n; i++) {
+        gets(name[i]);
+    }
+
+    char name_max[100];
+    strcpy(name_max, name[0]);
+
+    for(int i = 1; i < n; i++) {
+        if(strlen(name_max) < strlen(name[i])) {
+            strcpy(name_max, name[i]);
+        }
+    }
+
+    printf("%s", name_max);
+    return 0;
+}
+
