93c30fb27e5c4df82a36f46565ae3828ef31c020
yjq <blues@qq.com>
2020-06-28 20:13:16 +0800

    debug 1 by yjq

---
+++ 10/1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c = 0, d;
+    scanf("%d", &a);
+    
+    for(d = 2; d <= a; d += 1) {
+        c = 0;
+        for(b = 2; b * b <= d; b += 1) {
+            if(d % b == 0) {
+                c += 1;
+            }
+        }
+        if(c == 0) {
+            printf("%d\n", d);
+        }
+    }
+    
+    return 0;
+}
+
