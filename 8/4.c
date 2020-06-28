b05bedb07ca5474b16dae992867be923a906e706
yjq <blues@qq.com>
2020-06-28 18:11:39 +0800

    debug 4 by yjq

---
+++ 8/4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+    getchar();
+    char name[n][100];
+    char temp[100];
+
+    for(int i = 0; i < n; i++) {
+        gets(name[i]);
+    }
+
+    for (int j = 0; j < n; j++) {
+        for (int k = 0; k < n - j - 1; k++) {
+            char temp[20];
+            if((strlen(name[k]) < strlen(name[k + 1]))) {
+                strcpy(temp, name[k]);
+                strcpy(name[k], name[k + 1]);
+                strcpy(name[k + 1], temp);
+            }
+        }
+    }
+    
+    printf("%s", name[0]);
+    return 0;
+}
+
