79a96518355b482a07b1dbb94732cc249ab176dc
yjq <blues@qq.com>
2020-06-28 20:24:55 +0800

    debug 5 by yjq

---
+++ 14/5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, i, j, m;
+    scanf ("%d", &n);
+    
+    for (i = 1; i <= n; i++) {
+        for (j = 1, m = n + 1 - i; j <= (n - i + 1); j++, m--) {
+            printf ("%d", m);
+            if (j != (n - i + 1)) {
+                printf (" ");
+            } else if(i != n) {
+                printf("\n");
+            }
+        }
+    }
+    
+    return 0;
+}
+
