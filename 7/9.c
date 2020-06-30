5f314ae5448bc42cba135ddfa6e5d8de7a116bcc
sati370 <60167827+sati370@users.noreply.github.com>
2020-06-30 14:58:20 +0800

    debug 7-9again

--- 7/9.c
+++ 7/9.c
@@ -9,6 +9,7 @@ int main()
         if((y % 4 == 0 && y % 100 != 0 ) || y % 400 == 0) {
             if(d >= 1 && d <= 29)
                 printf("YES");
+            else printf("NO");
         } else {
             if(d >= 1 && d <= 28)
                 printf("YES");
@@ -16,19 +17,20 @@ int main()
                 printf("NO");
         }
     }
-    if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+    else if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
         if(d >= 1 && d <= 31)
             printf("YES");
         else
             printf("NO");
     }
-    if(m == 4 || m == 6 || m == 9 || m == 11 ) {
+    else if(m == 4 || m == 6 || m == 9 || m == 11 ) {
         if(d >= 1 && d <= 30)
             printf("YES");
         else
             printf("NO");
     }
-
+        else 
+            printf("NO"); 
     return 0;
 }
 
