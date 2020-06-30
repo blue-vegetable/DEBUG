b81934bf7fdf8dd88f2855398a4a4243d618aad3
blue-vegetable <61568090+blue-vegetable@users.noreply.github.com>
2020-06-30 17:38:01 +0800

    Update 9.c

--- 12/9.c
+++ 12/9.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-    float r;
-    float c, s;
-    scanf("%f", &r);
+    double r;
+    double c, s;
+    scanf("%lf", &r);
 
     c = 2 * PI * r;
     s = PI * r * r;
-    printf("%.3f\n", c);
-    printf("%.3f\n", s);
+    printf("%.3lf\n", c);
+    printf("%.3lf\n", s);
 
     return 0;
 }
