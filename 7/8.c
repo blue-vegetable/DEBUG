858dbce1f1b5fddb6c24aaaa9c964e1df213f31d
sati370 <60167827+sati370@users.noreply.github.com>
2020-06-30 15:17:43 +0800

    debug 7-8again

--- 7/8.c
+++ 7/8.c
@@ -4,13 +4,13 @@ int main()
 {
     int y, m, d;
     scanf("%d-%d-%d", &y, &m, &d);
-
-    if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
-        if(d <= 31) {
-            printf("YES");
-        } else {
-            printf("NO");
-        }
+    if(m>12) printf("NO")
+    else if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+            if(d <= 31) {
+                printf("YES");
+            } else {
+                printf("NO");
+            }
     } else if(m != 2) {
         if(d <= 30) {
             printf("YES");
