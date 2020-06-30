a58079e9184894ea881919feebe457fbe0ca7dbe
blue-vegetable <61568090+blue-vegetable@users.noreply.github.com>
2020-06-30 17:27:57 +0800

    Update 9.c

--- 8/9.c
+++ 8/9.c
@@ -6,10 +6,10 @@ int main()
     int n;
     int i, j;
     scanf("%d\n", &n);
-    char string[n][101];
+    char string[n][110];
 
     for (i = 0; i < n; i++) {
-        for (j = 0; j < 101; j++) {
+        for (j = 0; j < 110; j++) {
             scanf("%c", &string[i][j]);
             if (string[i][j] == '\n') {
                 string[i][j] = '\0';
@@ -20,13 +20,15 @@ int main()
 
     char str[200];
     for (i = 0; i < n - 1; i++) {
-        if (strlen(string[i]) > strlen(string[i + 1])) {
-            strcpy(str, string[i]);
-            strcpy(string[i], string[i + 1]);
-            strcpy(string[i + 1], str);
+        for(int j = 0; j < n - i - 1; j++) {
+            if (strlen(string[j]) < strlen(string[j + 1])) {
+                strcpy(str, string[j]);
+                strcpy(string[j], string[j + 1]);
+                strcpy(string[j + 1], str);
+            }
         }
     }
 
-    printf("%s", string[n - 1]);
+    printf("%s", string[0]);
     return 0;
 }
