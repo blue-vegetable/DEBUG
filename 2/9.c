1482cb13fa1e5f12c7603706ffd7598a56fc1b69
sati370 <60167827+sati370@users.noreply.github.com>
2020-06-30 16:07:42 +0800

    debug 2-9 again

--- 2/9.c
+++ 2/9.c
@@ -23,9 +23,11 @@ int main()
     }
     for (i = 0; i < m; i++) {
         for(j = 0; j < m; j++) {
+            int t = 0;
             for (k = 0; k < n; k++) {
-                matrix_c[i][j] += (matrix_a[i][k] * matrix_b[k][j]);
+                t += (matrix_a[i][k] * matrix_b[k][j]);
             }
+            matrix_c[i][j] = t;
         }
     }
     for (i = 0; i < m; i++) {
